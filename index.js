edfc04cb5c037b149a3bd68d71591bb74fa12ba8
jeferson-campos <jefersoncampos5296@gmail.com>
2024-10-14 12:25:11 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// let n1 = parseFloat(prompt("Digite o primeiro número:"));
+// let n2 = parseFloat(prompt("Digite o segundo número:"));
+// if (n1 > 0 && n2 > 0) {
+//     console.log("Ambos os números são positivos.");
+// }
+// else if (n1 > 0 || n2 > 0) {
+//     console.log("Pelo menos um dos números é positivo.");
+// }
+// else {
+//     console.log("Nenhum dos números é positivo.");
+// }
+
+// let n1 = parseFloat(prompt("Digite o primeiro número:"));
+// let n2 = parseFloat(prompt("Digite o segundo número:"));
+// console.log("Número 1 é igual ao Número 2? " + (n1 == n2));
+// console.log("Número 1 é diferente do Número 2? " + (n1 != n2));
